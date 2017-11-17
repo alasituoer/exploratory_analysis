ede14043ce858a3e263e235c9c1aae55c3c39064
alas <mingzhi_6143@163.com>
2017-11-17 19:09:58 +0800

    upload

--- code/_3rd_code/missing_value_statistics.py
+++ code/_3rd_code/missing_value_statistics.py
@@ -4,16 +4,10 @@ import matplotlib.pyplot as plt
 import time
 import numpy as np
 
-if __name__ == "__main__":
-    print "hello alas"
-    working_space = "C:/Users/cherish/Desktop/exploratory_analysis/"
-    path_file = working_space + "data/_3rd_data/"
-    filename = "telephone_detail_info.txt"
+def selectedIndexTelDetailInfo(path_file, filename):
+    """拆分混合字段为多列, 删除掉拆分过的字段, 导出新的文件"""
     df_all_cat = pd.read_table(path_file + filename)
-    #filename = "sample_detail_info.csv"
-    #df_all_cat = pd.read_csv(path_file + filename)
-    #print df_all_cat
-
+    filename = "sample_detail_info.csv"
     list_mix_index = ['C6_VALUE', 'C7_VALUE', 'C8_VALUE', 'Ui_VALUE',
             'D1_VALUE', 'D2_VALUE', 'D3_VALUE',
             'D9_VALUE', 'D10_VALUE', 'D11_VALUE',]
@@ -27,39 +21,47 @@ if __name__ == "__main__":
         df_all_cat = pd.concat([df_all_cat, df_split_cat], axis=1)
 
     df_all_cat.drop(list_mix_index, axis=1, inplace=True)    
-#    path_to_write = path_file + "counts_NA.csv"
-#    df_all_cat.count().to_csv(path_to_write)
-
-    #print df_all_cat
-    path_to_save = path_file + "NA_boxplot/"
-    print df_all_cat.columns[4:]
-    print len(df_all_cat.columns[4:])
-
-    series_list = []
-    for v in df_all_cat.columns[4:]:
-        try:
-            df_one_cat = df_all_cat[[v]].dropna(axis=0, how='any')
-            #print df_one_cat
-            series_list.append(df_one_cat.describe())
-        except Exception, e:
-            print e
-            continue
-    df_desc_all = pd.concat(series_list, axis=1)
-    #print df_desc_all
-    
-    df_desc_all.to_csv(path_file + "NA_Desc_ALL.csv")
+    df_all_cat.to_csv(path_file +\
+            "telephone_detail_info_reviewed.csv", index=False)
 
 
-"""
-            df_one_cat.boxplot()
-            plt.savefig(path_to_save + v + '.png')
-            #plt.show()
-            print v
-            time.sleep(3)
-        except Exception, e:
-            print e
-            continue
-"""
+def computeDescTelDetailInfo(path_file, filename):
+    df = pd.read_csv(path_file + filename)
+
+    series_desc_list = []
+    for v in df.columns[4:]:
+        #df[v].describe()
+        series_desc_list.append(df[[v]].describe())
+
+    # 将所有字段的描述性统计值横向拼接后输出
+    df_desc_all = pd.concat(series_desc_list, axis=1)
+    df_desc_all.to_csv(path_file + "desc_del_detail_info.csv")
+
+
+def saveBoxplotFigTelDetailInfo(path_file, filename):
+    df = pd.read_csv(path_file + filename)
+    # 挨个字段画出并存储箱线图
+    for v in df.columns[4:]:
+        df[[v]].boxplot()
+        plt.savefig(path_file + "NA_boxplot/" + v + ".png")
+        plt.show()
+
+
+if __name__ == "__main__":
+    print "hello alas"
+    working_space = "C:/Users/cherish/Desktop/exploratory_analysis/"
+    path_file = working_space + "data/_3rd_data/"
+
+#    filename1 = "telephone_detail_info.txt"
+#    selectedIndexTelDetailInfo(path_file, filename1)
+
+    filename2 = "telephone_detail_info_reviewed.csv"
+    computeDescTelDetailInfo(path_file, filename2)
+
+
+    filename3 = "telephone_detail_info_reviewed.csv"
+    saveBoxplotFigTelDetailInfo(path_file, filename3)
+
 
 """
             # 去掉异常值, 防止箱线图受极度异常值的影响
